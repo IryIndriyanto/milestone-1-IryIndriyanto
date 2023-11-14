dcbaa76065aba6e8f81706e961a2924fed5105aa
Iry Indriyanto <iry.indriyanto20@gmail.com>
2023-11-14 08:23:16 -0700

    adding content to main

--- script/index.js
+++ script/index.js
@@ -5,3 +5,14 @@ function openNav() {
   function closeNav() {
     document.getElementById("menu-mobile").style.display = "none";
   }
+
+  // navbar scroll effect
+  const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
